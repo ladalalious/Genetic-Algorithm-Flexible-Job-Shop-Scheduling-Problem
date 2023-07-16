12ad9bfb19f3a64f7b68606aae0e7e8e08e241aa
yujie <951280040@qq.com>
2023-07-17 00:21:04 +0800

    '不懂'

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from deap import base, creator
+from deap import base, creator,tools
+import random
+
+
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
-import random
-from deap import tools
+
+print(creator)
 
 IND_SIZE = 10
 
@@ -23,40 +26,40 @@ toolbox.register("evaluate", evaluate)
 
 
 
-def main():
-    pop = toolbox.population(n=50)
-    CXPB, MUTPB, NGEN = 0.5, 0.2, 40
+# def main():
+#     pop = toolbox.population(n=50)
+#     CXPB, MUTPB, NGEN = 0.5, 0.2, 40
 
-    # Evaluate the entire population
-    fitnesses = map(toolbox.evaluate, pop)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
+#     # Evaluate the entire population
+#     fitnesses = map(toolbox.evaluate, pop)
+#     for ind, fit in zip(pop, fitnesses):
+#         ind.fitness.values = fit
 
-    for g in range(NGEN):
-        # Select the next generation individuals
-        offspring = toolbox.select(pop, len(pop))
-        # Clone the selected individuals
-        offspring = map(toolbox.clone, offspring)
+#     for g in range(NGEN):
+#         # Select the next generation individuals
+#         offspring = toolbox.select(pop, len(pop))
+#         # Clone the selected individuals
+#         offspring = map(toolbox.clone, offspring)
 
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < CXPB:
-                toolbox.mate(child1, child2)
-                del child1.fitness.values
-                del child2.fitness.values
+#         # Apply crossover and mutation on the offspring
+#         for child1, child2 in zip(offspring[::2], offspring[1::2]):
+#             if random.random() < CXPB:
+#                 toolbox.mate(child1, child2)
+#                 del child1.fitness.values
+#                 del child2.fitness.values
 
-        for mutant in offspring:
-            if random.random() < MUTPB:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
+#         for mutant in offspring:
+#             if random.random() < MUTPB:
+#                 toolbox.mutate(mutant)
+#                 del mutant.fitness.values
 
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
+#         # Evaluate the individuals with an invalid fitness
+#         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+#         fitnesses = map(toolbox.evaluate, invalid_ind)
+#         for ind, fit in zip(invalid_ind, fitnesses):
+#             ind.fitness.values = fit
 
-        # The population is entirely replaced by the offspring
-        pop[:] = offspring
+#         # The population is entirely replaced by the offspring
+#         pop[:] = offspring
 
-    return pop
+#     return pop
